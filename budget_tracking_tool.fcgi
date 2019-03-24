238d864035c7e3f15bf12526e2e39feafc5808a8
= <=>
2019-03-24 16:29:01 -0400

    Getting site ready for google app engine.

--- budget_tracking_tool.fcgi
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-from flup.server.fcgi import WSGIServer
-from budget_tracking_tool import create_application
-
-if __name__ == '__main__':
-    app = create_application("Production")
-    WSGIServer(app).run()
--- make_pkg.sh
+++ make_pkg.sh
@@ -14,14 +14,13 @@ mkdir $BUDGET_SITE_DIRECTORY
 #Copy everything over to the temporary directory structure
 cp $BUDGET_TRACKER_CERTIFICATE $CERTIFICATE_DIRECTORY
 cp -r budget_tracking_tool/ $BUDGET_SITE_DIRECTORY
-cp budget_tracking_tool.fcgi $BUDGET_SITE_DIRECTORY
-cp Requirements*.txt $TEMP_DIRECTORY
+cp requirements.txt $TEMP_DIRECTORY
 
 #Create a text file with the new environment variables to be loaded by the .bashrc on the server.
-echo 'export BUDGET_TRACKER_CERTIFICATE="~/certificates/BudgetTracker-secret.json"' > $TEMP_DIR/budget_variables.sh
-echo 'export BUDGET_TRACKER_SECRET_KEY="$BUDGET_TRACKER_SECRET_KEY"' >> $TEMP_DIR/budget_variables.sh
+echo 'export BUDGET_TRACKER_CERTIFICATE="$HOME/certificates/BudgetTracker-secret.json"' > $TEMP_DIRECTORY/budget_variables.sh
+echo "export BUDGET_TRACKER_SECRET_KEY='$BUDGET_TRACKER_SECRET_KEY'" >> $TEMP_DIRECTORY/budget_variables.sh
+chmod 755 $TEMP_DIRECTORY/budget_variables.sh
 
-zip -r $PACKAGE $TEMP_DIRECTORY
-
-
-
+cd $TEMP_DIRECTORY
+zip -r $PACKAGE .
+mv $PACKAGE ..
